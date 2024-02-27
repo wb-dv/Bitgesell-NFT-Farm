cf393ec7e81d5445e8a6fc6f006d30860468b873
wb-dv <isss.300@mail.ru>
2024-02-27 22:49:25 +0300

    restore global.js, get by link from global and update index.html

--- src/components/UI/ByLink/ByLink.jsx
+++ src/components/UI/ByLink/ByLink.jsx
@@ -4,17 +4,20 @@ import { combineClasses } from '@helpers/combineClasses';
 
 import styles from './ByLink.module.scss';
 
-export const ByLink = memo(function ByLink({ customClasses }) {
-  const byLinkClasses = combineClasses([styles.ByLink], customClasses);
+const link =
+    window.byLink || 'https://opensea.io/collection/new-bitgesell-road';
 
-  return (
-    <a
-      className={byLinkClasses}
-      href="https://opensea.io/collection/new-bitgesell-road"
-      target="_blank"
-      rel="noreferrer"
-    >
-      get WBGL
-    </a>
-  );
+export const ByLink = memo(function ByLink({ customClasses }) {
+    const byLinkClasses = combineClasses([styles.ByLink], customClasses);
+
+    return (
+        <a
+            className={byLinkClasses}
+            href={link}
+            target='_blank'
+            rel='noreferrer'
+        >
+            get WBGL
+        </a>
+    );
 });
